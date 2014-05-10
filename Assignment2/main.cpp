0437f81c32188ba813e80d4b8465ec6e8c6e9815
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2014-05-09 20:32:47 -0700

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Benjamin Hendricks on 5/9/14.
+//  Copyright (c) 2014 Benjamin Hendricks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
