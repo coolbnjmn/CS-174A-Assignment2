92b2ad0a8964b89f750068b2de8e8109793f182f
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2014-05-12 21:57:49 -0700

    fixes two added shader files

--- Assignment2/anim.cpp
+++ Assignment2/anim.cpp
@@ -473,11 +473,15 @@ void drawPlayer(mat4 model_trans, mat4 view_trans) {
     model_trans = mvstack.pop();
     
     // draw right arm
-    model_trans *= Translate(1,0,0);
+    model_trans *= RotateY(90);
+    model_trans *= Scale(0.25, 0.1, 1.5);
+    model_trans *= Translate(0,6,1.5);
     model_trans *= RotateX(45*sin(TIME));
     model_view = view_trans * model_trans;
     drawSphere();
-    model_trans *= Translate(1, 0, 0);
+    
+    model_trans *= RotateZ(90);
+    model_trans *= Translate(0, 0,1.5);
     model_trans *= RotateY(45*sin(TIME));
     model_view = view_trans * model_trans;
     drawSphere();
